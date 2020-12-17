d950c3cf64b903053f668f908ff6d1e871141ac8
Scyy <bamslizard3201@gmail.com>
2020-12-17 16:07:16 +0800

    Initial commit - base Plugin class

---
+++ src/main/java/me/Scyy/Util/GenericJavaPlugin/Plugin.java
@@ -0,0 +1,21 @@
+package me.Scyy.Util.GenericJavaPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    public void reload() {
+
+    }
+
+}
