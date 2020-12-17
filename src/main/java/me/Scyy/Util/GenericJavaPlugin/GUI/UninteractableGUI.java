727cbdcf68a2e8addb907f1c6373efd874d1ef8d
Scyy <bamslizard3201@gmail.com>
2020-12-18 06:54:24 +0800

    Added javadocs to UniteractableGUI

--- src/main/java/me/Scyy/Util/GenericJavaPlugin/GUI/UninteractableGUI.java
+++ src/main/java/me/Scyy/Util/GenericJavaPlugin/GUI/UninteractableGUI.java
@@ -6,18 +6,33 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class UninteractableGUI extends InventoryGUI {
 
+    /**
+     * Copies everything from the last GUI
+     * @param lastGUI the GUI pen before this one
+     * @param plugin the Plugin
+     * @param player the player viewing this GUI
+     */
     public UninteractableGUI(InventoryGUI lastGUI, Plugin plugin, Player player) {
         super(lastGUI, plugin, player, lastGUI.name, lastGUI.size);
 
         this.inventoryItems = lastGUI.getInventoryItems();
     }
 
+    /**
+     * Denies any sort of interaction with the GUI
+     * @param event The click event in the GUI
+     * @return this same GUI
+     */
     @Override
     public InventoryGUI handleClick(InventoryClickEvent event) {
         event.setCancelled(true);
         return this;
     }
 
+    /**
+     * Denies the ability to interact with the players own inventory
+     * @return false
+     */
     @Override
     public boolean allowPlayerInventoryEdits() {
         return false;
