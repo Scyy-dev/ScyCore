8bad19587378a4129927c60b9c764a2d7e5d28fb
Scyy <bamslizard3201@gmail.com>
2020-12-18 09:35:56 +0800

    Added utility methods to the Plugin class to assist using config

--- src/main/java/me/Scyy/Util/GenericJavaPlugin/Plugin.java
+++ src/main/java/me/Scyy/Util/GenericJavaPlugin/Plugin.java
@@ -1,8 +1,13 @@
 package me.Scyy.Util.GenericJavaPlugin;
 
 import me.Scyy.Util.GenericJavaPlugin.Config.ConfigManager;
+import me.Scyy.Util.GenericJavaPlugin.Config.Settings;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Plugin extends JavaPlugin {
 
@@ -35,6 +40,23 @@ public class Plugin extends JavaPlugin {
             sender.sendMessage("Error reloading! Check console for logs!");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Get the Settings for this plugin, each defined in config.yml
+     * @return the Settings
+     */
+    public Settings getSettings() {
+        return configManager.getSettings();
+    }
+
+    /**
+     * Provides a bit of information about the plugin
+     * @return the splash text
+     */
+    public List<String> getSplashText() {
+        return Arrays.asList(
+                ChatColor.translateAlternateColorCodes('&', "PLUGIN_NAME v" + getDescription().getVersion()), "Built by _Scyy");
     }
 
 }
