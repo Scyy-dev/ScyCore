5166776b500f9c8926a35a90d919d7e724afb6cb
Scyy <bamslizard3201@gmail.com>
2020-12-20 21:59:51 +0800

    Added a reload command for the Admin command

--- src/main/java/me/Scyy/Util/GenericJavaPlugin/Command/AdminCommand.java
+++ src/main/java/me/Scyy/Util/GenericJavaPlugin/Command/AdminCommand.java
@@ -1,21 +1,65 @@
 package me.Scyy.Util.GenericJavaPlugin.Command;
 
+import me.Scyy.Util.GenericJavaPlugin.Config.PlayerMessenger;
+import me.Scyy.Util.GenericJavaPlugin.Plugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AdminCommand implements TabExecutor {
 
+    private final Plugin plugin;
+
+    private final PlayerMessenger pm;
+
+    public AdminCommand(Plugin plugin) {
+        this.plugin = plugin;
+        this.pm = plugin.getConfigManager().getPlayerMessenger();
+    }
+
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) { }
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        if (args.length == 0) {
+            for (String message : plugin.getSplashText()) {
+                sender.sendMessage(message);
+                return true;
+            }
+        }
+
+        // IDE flags this with a warning for not enough case statements - ignored due to more commands expected when plugin is created
+        switch (args[0]) {
+            case "reload":
+                if (!sender.hasPermission("plugin.admin.reload")) {
+                    pm.msg(sender, "errorMessages.noPermission"); return true;
+                }
+                plugin.reload(sender);
+                return true;
+            default:
+                pm.msg(sender, "errorMessages.invalidCommand");
+                return true;
+        }
+
+    }
 
     @Nullable
     @Override
-    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        return null;
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        // IDE flags this with a warning for not enough case statements - ignored due to more commands expected when plugin is created
+        switch (args.length) {
+            case 1:
+                if (sender.hasPermission("plugin.admin.reload")) list.add("reload");
+                return list;
+            default:
+                return list;
+        }
     }
 }
