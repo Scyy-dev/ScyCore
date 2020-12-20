a891e536d646e095f4c678fb199f51a650e7bdb4
Scyy <bamslizard3201@gmail.com>
2020-12-20 21:19:45 +0800

    Initial commit for the Admin command

---
+++ src/main/java/me/Scyy/Util/GenericJavaPlugin/Command/AdminCommand.java
@@ -0,0 +1,21 @@
+package me.Scyy.Util.GenericJavaPlugin.Command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class AdminCommand implements TabExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) { }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return null;
+    }
+}
