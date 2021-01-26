c34177d7241b297023f894dcfba4426d20b11c03
Scyy <bamslizard3201@gmail.com>
2021-01-26 21:55:01 +0800

    Updated ConfigManager to allow for better handling of errors when reloading configs

--- src/main/java/me/Scyy/Util/GenericJavaPlugin/Config/ConfigManager.java
+++ src/main/java/me/Scyy/Util/GenericJavaPlugin/Config/ConfigManager.java
@@ -1,4 +1,6 @@
-package me.Scyy.Util.GenericJavaPlugin.Config.Managers;
+package me.Scyy.Util.GenericJavaPlugin.Config;
+
+import me.Scyy.Util.GenericJavaPlugin.Plugin;
 
 /**
  * Manager for a collection of config files. Recommended to provide methods for getting each of the ConfigFiles it manages
@@ -8,6 +10,8 @@ public interface ConfigManager {
     /**
      * Reloads all configs this manager is responsible for
      */
-    void reloadConfigs();
+    void reloadConfigs() throws Exception;
+
+    Plugin getPlugin();
 
 }
