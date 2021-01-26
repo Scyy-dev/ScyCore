c336cdc751b00936f69cc531cc32b2eeaf6f7e28
Scyy <bamslizard3201@gmail.com>
2021-01-26 22:02:23 +0800

    Updated Settings to reflect ConfigFile changes

--- src/main/java/me/Scyy/Util/GenericJavaPlugin/Config/Settings.java
+++ src/main/java/me/Scyy/Util/GenericJavaPlugin/Config/Settings.java
@@ -1,16 +1,13 @@
 package me.Scyy.Util.GenericJavaPlugin.Config;
 
-import me.Scyy.Util.GenericJavaPlugin.Plugin;
-
 public class Settings extends ConfigFile {
 
     /**
      * Create a ConfigFile for the default 'config.yml' file
      * Intended to be used as a read-only file, it is highly recommended that
      *  {@link org.bukkit.configuration.file.YamlConfiguration#set(String, Object)} is not used on this file as comments will be overwritten
-     * @param plugin the Plugin
      */
-    public Settings(Plugin plugin) {
-        super(plugin, plugin.getConfigManager(), "config.yml");
+    public Settings(ConfigManager manager) {
+        super(manager,"config.yml", true);
     }
 }
