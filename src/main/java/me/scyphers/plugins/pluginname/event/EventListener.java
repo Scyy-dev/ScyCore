ff2d8b1099935788bbf9c1e1e1230b22e38960fd
Scyy <bamslizard3201@gmail.com>
2021-08-03 17:01:37 +0800

    converted the event listener to a record

--- src/main/java/me/scyphers/plugins/pluginname/event/EventListener.java
+++ src/main/java/me/scyphers/plugins/pluginname/event/EventListener.java
@@ -3,12 +3,6 @@ package me.scyphers.plugins.pluginname.event;
 import me.scyphers.plugins.pluginname.Plugin;
 import org.bukkit.event.Listener;
 
-public class EventListener implements Listener {
-
-    private final Plugin plugin;
-
-    public EventListener(Plugin plugin) {
-        this.plugin = plugin;
-    }
+public record EventListener(Plugin plugin) implements Listener {
 
 }
