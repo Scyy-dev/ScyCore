0fdede99d3475f82669f203924cc1c769e1761c9
Scyy <bamslizard3201@gmail.com>
2021-08-03 21:34:42 +0800

    Made the Plugin class final to prevent unintended behaviour from external plugins

--- src/main/java/me/scyphers/plugins/pluginname/Plugin.java
+++ src/main/java/me/scyphers/plugins/pluginname/Plugin.java
@@ -13,7 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Arrays;
 import java.util.List;
 
-public class Plugin extends JavaPlugin {
+public final class Plugin extends JavaPlugin {
 
     private SimpleFileManager configManager;
 
