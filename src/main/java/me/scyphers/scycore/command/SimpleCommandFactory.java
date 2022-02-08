1b5694e369febf47375f6d8ec4a1bd4e4a37abf3
Scyy <bamslizard3201@gmail.com>
2022-02-08 13:43:52 +0800

    SimpleCommandFactory now retrieves and registers the command listener

--- src/main/java/me/scyphers/scycore/command/SimpleCommandFactory.java
+++ src/main/java/me/scyphers/scycore/command/SimpleCommandFactory.java
@@ -1,7 +1,9 @@
 package me.scyphers.scycore.command;
 
+import me.scyphers.scycore.BasePlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,8 +15,15 @@ public abstract class SimpleCommandFactory implements TabExecutor {
 
     private final Consumer<CommandSender> noArgConsumer;
 
-    public SimpleCommandFactory(Consumer<CommandSender> noArgConsumer) {
+    public SimpleCommandFactory(BasePlugin plugin, String baseCommandName, Consumer<CommandSender> noArgConsumer) {
         this.noArgConsumer = noArgConsumer;
+        PluginCommand command = plugin.getCommand(baseCommandName);
+        if (command == null) {
+            throw new IllegalArgumentException("Could not find command with name " + baseCommandName);
+        } else {
+            command.setExecutor(this);
+            command.setTabCompleter(this);
+        }
     }
 
     @Override
